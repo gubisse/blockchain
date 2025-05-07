640efe65f9e06e10b5c380cdbd019fe415550728
Gonsalves Ubisse <goncalvesalbertoubisse@gmail.com>
2025-05-07 12:20:21 +0200

    Atualizacao ...

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,19 +3,16 @@ import { Header } from "~/components/Header";
 import { Footer } from "~/components/Footer";
 
 export default component$(() => {
-  const carregando = useSignal(true);
 
   return (
     <>
-      <Header />
-      {carregando.value &&        
+      <Header />      
         <div class="flex min-h-screen items-center justify-center bg-gray-100">
           <div class="bg-white p-8 rounded-2xl shadow-lg text-center w-full max-w-md">
-            <p class="text-gray-600 mb-6">Estamos carregando seu ambiente personalizado...</p>
+            <p class="text-gray-600 mb-6">Seja Bem-Vindo na Plataforma Blockchain, para calculo de reagentes. </p>
             <div class="animate-spin rounded-full h-10 w-10 border-t-4 border-blue-500 border-solid mx-auto"></div>
           </div>
         </div>
-      }
       <Footer />
     </>
 
